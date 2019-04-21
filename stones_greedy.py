819cef0c2b64508b51e67709e1a183c4ad91d403
esparig <esparig@gmail.com>
2019-04-21 15:18:54 +0200

    Update stones_greedy.py

--- stones_greedy.py
+++ stones_greedy.py
@@ -1,3 +1,12 @@
+def get_energy(s, stones):
+    energy = 0
+    t = 0
+    for e, l in stones:
+        e0 = e - (t*l)
+        if e0 > 0:
+            energy += e0
+        t += s
+    return energy
 # input() reads a string with a line of input, stripping the ' ' (newline) at the end.
 # This is all you need for most Code Jam problems.
 t = int(input()) # read a line with a single integer
@@ -7,9 +16,5 @@ for i in range(1, t + 1):
     for j in range(n):
         s, e, l = [int(s) for s in input().split(" ")] # read a list of integers, 3 in this case
         stones.append((e, l))
-    st = sorted(stones, key=lambda x: x[1])
-    print(st)
-    print(stones)
-    print(sorted( st, reverse=True))
-    print("Case #{}: {}".format(i, 0))
-# check out .format's specification for more formatting options
+    print("Case #{}: {}".format(i, get_energy(s, sorted( sorted(stones, key=lambda x: x[1]), key=lambda x: x[0], reverse=True))))
+    # check out .format's specification for more formatting options
